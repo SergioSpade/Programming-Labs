aa0decb859ef994f045c22d73e6e8a00b49c4464
Sergio Castro Jr <SergioSpade@users.noreply.github.com>
2017-12-15 16:43:44 -0500

    Almost Chess. Chapter 5 Lab

--- Almost Chess/Almost Chess/Properties/AssemblyInfo.cs
+++ Almost Chess/Almost Chess/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Final Exam Program")]
+[assembly: AssemblyTitle("Almost Chess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Final Exam Program")]
+[assembly: AssemblyProduct("Almost Chess")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b163054d-7e9b-4b4c-ba25-13384ba59287")]
+[assembly: Guid("1ae8fa52-66f8-46c3-879f-9b73245b612c")]
 
 // Version information for an assembly consists of the following four values:
 //
